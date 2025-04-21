144e861c9a96f361bc0d52dffeec50c146cba0e4
septiadiassep <septiadiassep@gmail.com>
2025-04-22 02:21:42 +0700

    Feat: menambahkan 2 router store dan delete data User

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/users', [UserController::class, 'index'])->name('user.index');
+Route::post('/users/store', [UserController::class, 'store'])->name('user.store');
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
